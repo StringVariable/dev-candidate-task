0fa84f43b595a5102ea7ca8e4dd36f94bf0aec8a
Andrew O'Rourke <andrew.john.orourke@gmail.com>
2019-04-19 17:28:16 +1000

    UPDATE: Styled the header as to design spec.

--- app/src/Components/Header/Header.js
+++ app/src/Components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './header.scss';
+import logo from '../../Assets/logo.png';
 
 const Header = () => (
-    <div>Header here</div>
+    <div className="header">
+      <img src={logo} alt="Askable logo" className="logo"/>
+    </div>
 );
 
 export default Header;
